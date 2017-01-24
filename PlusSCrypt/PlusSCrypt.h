e729baa8e072f620c51cdc8f28f491b8dbf591f4
Matej Ukmar <matejukmar@gmail.com>
2017-01-24 15:32:42 +0530

    Added missing files

---
+++ PlusSCrypt/PlusSCrypt.h
@@ -0,0 +1,21 @@
+//
+//  PlusSCrypt.h
+//  PlusSCrypt
+//
+//  Created by Matej Ukmar on 24/01/2017.
+//  Copyright © 2017 ZEN+. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlusSCrypt.
+FOUNDATION_EXPORT double PlusSCryptVersionNumber;
+
+//! Project version string for PlusSCrypt.
+FOUNDATION_EXPORT const unsigned char PlusSCryptVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlusSCrypt/PublicHeader.h>
+
+
+#import "libscrypt.h"
+
