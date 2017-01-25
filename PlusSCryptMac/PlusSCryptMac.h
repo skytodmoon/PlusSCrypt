10250c10bb8c86bd9c0858f553fe25df4df0dc02
Matej Ukmar <matejukmar@gmail.com>
2017-01-25 18:29:59 +0530

    added mac target

---
+++ PlusSCryptMac/PlusSCryptMac.h
@@ -0,0 +1,20 @@
+//
+//  PlusSCryptMac.h
+//  PlusSCryptMac
+//
+//  Created by Matej Ukmar on 24/01/2017.
+//  Copyright © 2017 ZEN+. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PlusSCryptMac.
+FOUNDATION_EXPORT double PlusSCryptMacVersionNumber;
+
+//! Project version string for PlusSCryptMac.
+FOUNDATION_EXPORT const unsigned char PlusSCryptMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlusSCryptMac/PublicHeader.h>
+
+
+#import "libscrypt.h"
